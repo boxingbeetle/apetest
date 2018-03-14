a5af0c5914808be8311807e90c4f15d81507fd20
Maarten ter Huurne <maarten@treewalker.org>
2018-03-14 08:02:09 +0100

    SoftFab plugin: Remove commented out code
    The mid-level data can be passed in the same properties file as the
    main execution result, so the code to write it to a separate file
    is not needed anymore. Removing it permanently instead of commenting
    it out allows code simplifications.

--- ape/softfab.py
+++ ape/softfab.py
@@ -11,29 +11,16 @@ class PropertiesPlugin(Plugin):
     def postProcess(self, scribe):
         total = len(scribe.pages)
         numFailedPages = len(scribe.getFailedPages())
-        resultsProperties = {
+        data = {
             'result': 'ok' if numFailedPages == 0 else 'warning',
             'summary': scribe.getSummary(),
             'data.pages_total': total,
             'data.pages_pass': total - numFailedPages,
             'data.pages_fail': numFailedPages,
             }
-        #midLevelProperties =  {
-        #    'extraction.result': 'ok',
-        #    'data.pages_total': total,
-        #    'data.pages_pass': total - numFailedPages,
-        #    'data.pages_fail': numFailedPages,
-        #    }
-
-        for description, fileName, data in (
-            ( 'execution', 'results.properties', resultsProperties ),
-            #( 'mid-level', 'extracted.properties', midLevelProperties ),
-            ):
-            fullFileName = self.propertiesDir + '/' + fileName
-            print 'Writing %s metadata to "%s"...' % (
-                description, fullFileName
-                )
-            out = file(fullFileName, 'w')
-            for key in sorted(data.iterkeys()):
-                print >> out, '%s=%s' % ( key, data[key] )
-            out.close()
+        fullFileName = self.propertiesDir + '/results.properties'
+        print 'Writing metadata to "%s"...' % fullFileName
+        out = file(fullFileName, 'w')
+        for key in sorted(data.iterkeys()):
+            print >> out, '%s=%s' % ( key, data[key] )
+        out.close()

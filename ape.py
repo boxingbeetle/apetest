61ba515c67e45215673e2aadbc21bfc0e55d44f3
Maarten ter Huurne <maarten@treewalker.org>
2018-03-21 14:52:20 +0100

    Parse command line arguments using argparse module

--- ape.py
+++ ape.py
@@ -2,14 +2,31 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+from argparse import ArgumentParser
 import sys
 
 from ape.cmdline import run
 
+def main():
+    parser = ArgumentParser(
+        description='Automated Page Exerciser: '
+                    'smarter-than-monkey testing for web apps',
+        epilog='This is a test tool; do not use on production sites.'
+        )
+    parser.add_argument(
+        'url',
+        help='web app/site to check'
+        )
+    parser.add_argument(
+        'report',
+        help='file to write the HTML report to'
+        )
+    parser.add_argument(
+        'plugin', nargs='*',
+        help='name(#arg=value)*'
+        )
+    args = parser.parse_args()
+    sys.exit(run(args.url, args.report, *args.plugin))
+
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('Usage:')
-        print('  %s <URL> <report> (<plugin>(#<name>=<value>)*)*' % sys.argv[0])
-        sys.exit(2)
-    else:
-        sys.exit(run(*sys.argv[1:])) # pylint: disable=no-value-for-parameter
+    main()

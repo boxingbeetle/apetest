boxingbeetle/apetest

commit a160a8cf97b796489b97f90440afd94cd7dc42a0

Use defaultdict in Spider class
It is now used both to keep track of query counts and referrers.

ape/spider.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
+from collections import defaultdict
+
 class Spider(object):
     # TODO: Now just the first 100 are checked, it would be better to try
     #       variations of all query arguments.
@@ -8,11 +10,11 @@ class Spider(object):
     def __init__(self, firstRequest):
         self.requestsToCheck = set([firstRequest])
         self.requestsChecked = set()
-        self.queriesPerPage = {}
+        self.queriesPerPage = defaultdict(int)
         # Maps source request to referrers (destination).
         self.siteGraph = {}
         # Maps destination page to source requests.
-        self.pageReferredFrom = {}
+        self.pageReferredFrom = defaultdict(set)
 
     def __iter__(self):
         while self.requestsToCheck:
@@ -32,31 +34,24 @@ class Spider(object):
 
         for referrer in referrers:
             pageURL = referrer.pageURL
+            self.pageReferredFrom[pageURL].add(sourceRequest)
 
-            sources = self.pageReferredFrom.get(pageURL)
-            if sources is None:
-                self.pageReferredFrom[pageURL] = sources = set()
-            sources.add(sourceRequest)
-
-            count = self.queriesPerPage.get(pageURL, 0)
-            maxQueriesPerPage = self.maxQueriesPerPage
             for request in referrer.iterRequests():
                 if request in self.requestsChecked \
                 or request in self.requestsToCheck:
                     continue
-                if count >= maxQueriesPerPage:
+                if self.queriesPerPage[pageURL] >= self.maxQueriesPerPage:
                     print 'maximum number of queries reached for "%s"' % (
                         pageURL
                         )
                     break
-                count += 1
+                self.queriesPerPage[pageURL] += 1
                 self.requestsToCheck.add(request)
-            self.queriesPerPage[pageURL] = count
 
     def iterReferringRequests(self, destRequest):
         '''Iterate through the requests that refer to the given request.
         '''
-        for sourceRequest in self.pageReferredFrom.get(destRequest.pageURL, ()):
+        for sourceRequest in self.pageReferredFrom[destRequest]:
             for referrer in self.siteGraph[sourceRequest]:
                 if referrer.hasRequest(destRequest):
                     yield sourceRequest

commit fb16c3ffd7036f8111ecc854dc6040f183bde50c

Fixed iteration of referrers
The Request object was used as a key in a dictionary that contains
page URLs as keys.

This got broken in commit a160a8cf (Use defaultdict in Spider class).

ape/spider.py
@@ -49,7 +49,7 @@ class Spider:
     def iter_referring_requests(self, dest_req):
         '''Iterate through the requests that refer to the given request.
         '''
-        for source_req in self._page_referred_from[dest_req]:
+        for source_req in self._page_referred_from[dest_req.page_url]:
             for referrer in self._site_graph[source_req]:
                 if referrer.has_request(dest_req):
                     yield source_req
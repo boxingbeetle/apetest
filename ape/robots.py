5aa1063e02a4675e1cd7f258fd4cba7181aa1e50
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-01-14 09:24:24 +0100

    Add module for handling "robots.txt"
    It is not used yet by APE itself, but there is a unit test.

---
+++ ape/robots.py
@@ -0,0 +1,211 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Functions for handling robot exclusion files ("robots.txt").
+
+There is no official standard for these files: it started with a post
+that was later developed into a draft RFC, but that was never finalized.
+Later, several search engines invented their own extensions.
+
+This module implements all of the original proposal and some of the
+extensions:
+- "allow" rules
+- percent-encoded UTF-8 in paths
+
+Note that the module takes sequences of strings as its input,
+so the decoding of the text file itself is done by the caller.
+For interoperability, it is recommended to support at least
+UTF-8 both with and without a BOM.
+
+Features that are not (yet) implemented:
+- non-group records such as "sitemap"
+- "crawl-delay" rules
+- wildcards in paths
+
+https://en.wikipedia.org/wiki/Robots_exclusion_standard
+https://developers.google.com/search/reference/robots_txt
+http://www.robotstxt.org/
+"""
+
+def scan_robots_txt(lines, logger):
+    """Iterates through the records, where each record is a list of
+    (lineno, token, value) triples.
+    Problems are logged.
+    """
+    record = []
+    for lineno, line in enumerate(lines, 1):
+        stripped_line = line.lstrip()
+        if stripped_line.startswith('#'):
+            # Comment-only lines are discarded and do not end records.
+            continue
+        if not stripped_line:
+            # Empty lines end records.
+            if record:
+                yield record
+                record = []
+            continue
+        if len(stripped_line) != len(line):
+            logger.warning('Line %d has whitespace before field', lineno)
+
+        nocomment_line = stripped_line.split('#', 1)[0]
+        try:
+            field, value = nocomment_line.split(':', 1)
+        except ValueError:
+            logger.error('Line %d contains no ":"; ignoring line', lineno)
+        else:
+            record.append((lineno, field.casefold(), value.strip()))
+
+    if record:
+        yield record
+
+def parse_robots_txt(records, logger):
+    """Parses robots.txt records.
+    Returns a mapping from case-folded user agent name to allow/disallow
+    rules, where rules are a sequence of pair containing a bool (True iff
+    allowed) and a URL prefix string.
+    Problems are logged.
+    """
+    result = {}
+    unknowns = set()
+    for record in records:
+        seen_user_agent = False
+        rules = []
+        for lineno, field, value in record:
+            if field == 'user-agent':
+                if rules:
+                    logger.error(
+                        'Line %d specifies user agent after rules; '
+                        'assuming new record', lineno
+                        )
+                    rules = []
+                seen_user_agent = True
+                name = value.casefold()
+                if name in result:
+                    logger.error(
+                        'Line %d specifies user agent "%s", which was '
+                        'already addressed in an earlier record; '
+                        'ignoring new record', lineno, value
+                        )
+                else:
+                    result[name] = rules
+            elif field in ('allow', 'disallow'):
+                if seen_user_agent:
+                    try:
+                        path = unescape_path(value)
+                    except ValueError as ex:
+                        logger.error(
+                            'Bad escape in %s URL on line %d: %s',
+                            field, lineno, ex
+                            )
+                    else:
+                        # Ignore allow/disallow directives without a path.
+                        if path:
+                            rules.append((field == 'allow', path))
+                else:
+                    logger.error(
+                        'Line %d specifies %s rule without a preceding '
+                        'user agent line; ignoring line', lineno, field
+                        )
+            else:
+                # Unknown fields are allowed for extensions.
+                if field not in unknowns:
+                    unknowns.add(field)
+                    logger.info(
+                        'Unknown field "%s" (line %d)', field, lineno
+                        )
+    return result
+
+def unescape_path(path):
+    """Decode percent-encoded URL path.
+    Raise ValueError if the escaping is incorrect.
+    """
+    idx = 0
+    while True:
+        idx = path.find('%', idx)
+        if idx == -1:
+            return path
+
+        # Percent escaping can be used for UTF-8 paths.
+        start = idx
+        data = []
+        while True:
+            hex_num = path[idx + 1:idx + 3]
+            if len(hex_num) != 2:
+                raise ValueError(
+                    'incomplete escape, expected 2 characters after "%"'
+                    )
+            idx += 3
+
+            try:
+                if '-' in hex_num:
+                    raise ValueError()
+                value = int(hex_num, 16)
+            except ValueError:
+                raise ValueError(
+                    'incorrect escape: expected 2 hex digits after "%%", '
+                    'got "%s"' % hex_num
+                    )
+            data.append(value)
+
+            if len(data) > 1:
+                if (value & 0xC0) == 0x80:
+                    remaining -= 1 # pylint: disable=undefined-variable
+                    if remaining == 0:
+                        path = path[:start] + bytes(data).decode() + path[idx:]
+                        break
+                else:
+                    raise ValueError(
+                        'invalid percent-encoded UTF8: expected 0x80..0xBF '
+                        'for non-first byte, got 0x%02X' % value
+                        )
+            elif value == 0x2F: # '/'
+                # Path separator should remain escaped.
+                path = path[:start] + '%2f' + path[idx:]
+                break
+            elif value < 0x80:
+                path = path[:start] + chr(value) + path[idx:]
+                break
+            elif value < 0xC0 or value >= 0xF8:
+                raise ValueError(
+                    'invalid percent-encoded UTF8: expected 0xC0..0xF7 '
+                    'for first byte, got 0x%02X' % value
+                    )
+            elif value < 0xE0:
+                remaining = 1
+            elif value < 0xF0:
+                remaining = 2
+            elif value < 0xF8:
+                remaining = 3
+            else:
+                assert False, value
+
+            if idx == len(path) or path[idx] != '%':
+                raise ValueError(
+                    'incomplete escaped UTF8 character, '
+                    'expected %d more escaped bytes' % remaining
+                    )
+
+def lookup_robots_rules(rules_map, user_agent):
+    """Looks up a user agent in the rules map.
+    Returns a list of rules that apply to the given user agent.
+    """
+    user_agent = user_agent.casefold()
+    for name, rules in rules_map.items():
+        if name.startswith(user_agent):
+            return rules
+    return rules_map.get('*', [])
+
+def path_allowed(path, rules):
+    """Returns True iff `path` is allowed by `rules`.
+    The path should not contain percent-encoded values other than %2f ('/').
+    """
+    # The draft RFC specifies that the first match should be used,
+    # but both Google and Bing use the longest (most specific) match
+    # instead. This means that in practice "longest match" will be
+    # used by sites, so we'll follow that.
+    result = True
+    longest = 0
+    for allow, prefix in rules:
+        if path.startswith(prefix) and len(prefix) > longest:
+            result = allow
+            longest = len(prefix)
+    return result
---
+++ tests/test_robots.py
@@ -0,0 +1,274 @@
+import logging
+import unittest
+
+from ape.robots import (
+    lookup_robots_rules, parse_robots_txt, path_allowed, scan_robots_txt
+    )
+
+logger = logging.getLogger(__name__)
+
+class NoLogHandler(logging.Handler):
+    """Log handler that asserts if anything is logged.
+    """
+    LOGGING_FORMAT = '%(levelname)s: %(message)s'
+
+    def __init__(self):
+        logging.Handler.__init__(self)
+        self.setFormatter(logging.Formatter(self.LOGGING_FORMAT))
+
+    def __enter__(self):
+        logger.addHandler(self)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        logger.removeHandler(self)
+
+    def emit(self, record):
+        message = self.format(record)
+        assert False, 'Unexpected logging: %s' % message
+
+no_log = NoLogHandler()
+
+# Modified example from the Internet-Draft.
+
+EXAMPLE_LINES = '''
+User-agent: unhipbot
+Disallow: /
+
+User-agent: webcrawler
+User-agent: excite      # comment
+Disallow:
+
+User-agent: *
+Disallow: /org/plans.html
+Allow: /org/
+Allow: /serv
+# Comment-only lines do not end record.
+Allow: /~mak
+Disallow: /
+'''.split('\n')
+
+EXAMPLE_RECORDS = [
+    [(2, 'user-agent', 'unhipbot'),
+     (3, 'disallow', '/')],
+    [(5, 'user-agent', 'webcrawler'),
+     (6, 'user-agent', 'excite'),
+     (7, 'disallow', '')],
+    [(9, 'user-agent', '*'),
+     (10, 'disallow', '/org/plans.html'),
+     (11, 'allow', '/org/'),
+     (12, 'allow', '/serv'),
+     (14, 'allow', '/~mak'),
+     (15, 'disallow', '/')]
+    ]
+
+EXAMPLE_MAP = {
+    '*': [
+        (False, '/org/plans.html'),
+        (True, '/org/'),
+        (True, '/serv'),
+        (True, '/~mak'),
+        (False, '/')
+        ],
+    'unhipbot': [
+        (False, '/')
+        ],
+    'webcrawler': [],
+    'excite': [],
+    }
+
+class TestScanRobots(unittest.TestCase):
+    """Test scanning of "robots.txt".
+    """
+
+    def test_0100_empty(self):
+        """Test scanning of files that contain no records."""
+        with no_log:
+            self.assertCountEqual(scan_robots_txt([], logger), ())
+            self.assertCountEqual(scan_robots_txt([''], logger), ())
+            self.assertCountEqual(scan_robots_txt(['', ''], logger), ())
+            self.assertCountEqual(scan_robots_txt([' ', '\t'], logger), ())
+            self.assertCountEqual(scan_robots_txt(['#comment'], logger), ())
+
+    def test_0200_example(self):
+        """Test scanning of example file."""
+        with no_log:
+            self.assertEqual(
+                list(scan_robots_txt(EXAMPLE_LINES, logger)),
+                EXAMPLE_RECORDS
+                )
+
+    def test_0300_warn(self):
+        """Test scanning of files that trigger warnings."""
+        with self.assertLogs(logger, logging.WARNING):
+            self.assertEqual(list(scan_robots_txt([
+                # Whitespace before field
+                ' User-agent: *', 'Disallow: /'
+                ], logger)), [
+                [(1, 'user-agent', '*'), (2, 'disallow', '/')],
+                ])
+        with self.assertLogs(logger, logging.WARNING):
+            self.assertEqual(list(scan_robots_txt([
+                # Non-empty line without ":"
+                'User-agent: *', 'Foo', 'Disallow: /'
+                ], logger)), [
+                [(1, 'user-agent', '*'), (3, 'disallow', '/')],
+                ])
+
+class TestParseRobots(unittest.TestCase):
+    """Test parsing of "robots.txt".
+    """
+
+    def test_0100_empty(self):
+        """Test parsing of empty record set."""
+        with no_log:
+            self.assertEqual(parse_robots_txt((), logger), {})
+
+    def test_0200_example(self):
+        """Test parsing of example records."""
+        with no_log:
+            self.assertEqual(
+                parse_robots_txt(EXAMPLE_RECORDS, logger),
+                EXAMPLE_MAP
+                )
+
+    def test_0300_unknown(self):
+        """Test handling of unknown fields."""
+        records = [
+            [(1, 'user-agent', '*'),
+             (2, 'foo', 'bar'),
+             (3, 'disallow', '/')]
+            ]
+        with self.assertLogs(logger, logging.INFO):
+            self.assertEqual(
+                parse_robots_txt(records, logger),
+                {'*': [(False, '/')]}
+                )
+
+    def test_0310_user_argent_after_rules(self):
+        """Test handling of user agents specified after rules."""
+        records = [
+            [(1, 'user-agent', 'smith'),
+             (2, 'disallow', '/m'),
+             (3, 'user-agent', 'bender'),
+             (4, 'disallow', '/casino')]
+            ]
+        with self.assertLogs(logger, logging.ERROR):
+            self.assertEqual(
+                parse_robots_txt(records, logger),
+                {'smith': [(False, '/m')], 'bender': [(False, '/casino')]}
+                )
+
+    def test_0320_rules_before_user_argent(self):
+        """Test handling of rules specified before user agent."""
+        records = [
+            [(1, 'disallow', '/m'),
+             (2, 'user-agent', 'smith'),
+             (3, 'user-agent', 'bender'),
+             (4, 'disallow', '/casino')]
+            ]
+        with self.assertLogs(logger, logging.ERROR):
+            self.assertEqual(
+                parse_robots_txt(records, logger),
+                {'smith': [(False, '/casino')], 'bender': [(False, '/casino')]}
+                )
+
+    def test_0330_duplicate_user_agent(self):
+        """Test handling of multiple rules for the same user agent."""
+        records = [
+            [(1, 'user-agent', 'smith'),
+             (2, 'disallow', '/m2'),
+             (3, 'user-agent', 'smith'),
+             (4, 'disallow', '/m3')]
+            ]
+        with self.assertLogs(logger, logging.ERROR):
+            self.assertEqual(
+                parse_robots_txt(records, logger),
+                {'smith': [(False, '/m2')]}
+                )
+
+    def test_0400_unescape_valid(self):
+        """Test unescaping of correctly escaped paths."""
+        records = [
+            [(1, 'user-agent', '*'),
+             (2, 'disallow', '/a%3cd.html'),
+             (3, 'disallow', '/%7Ejoe/'),
+             (4, 'disallow', '/a%2fb.html'),
+             (5, 'disallow', '/%C2%A2'),
+             (6, 'disallow', '/%e2%82%ac'),
+             (7, 'disallow', '/%F0%90%8d%88')]
+            ]
+        with no_log:
+            self.assertEqual(parse_robots_txt(records, logger), {
+                '*': [
+                    (False, '/a<d.html'),
+                    (False, '/~joe/'),
+                    (False, '/a%2fb.html'),
+                    (False, '/\u00A2'),
+                    (False, '/\u20AC'),
+                    (False, '/\U00010348'),
+                    ]
+                })
+
+    def test_0410_unescape_invalid(self):
+        """Test handling of incorrect escaped paths."""
+        for bad_path in (
+                '/%', '/%1', # too short
+                '/%1x', '/%-3', # bad hex digits
+                '/%80', '/%e2%e3', # not UTF8
+                '/%e2%82', '/%e2%82%a', '/%e2%82ac', # incomplete UTF8
+                ):
+            with self.assertLogs(logger, logging.ERROR):
+                self.assertEqual(
+                    parse_robots_txt([
+                        [(1, 'user-agent', '*'),
+                         (2, 'disallow', bad_path),
+                         (3, 'allow', '/good')]
+                        ], logger),
+                    {'*': [(True, '/good')]}
+                    )
+
+    def test_0500_lookup(self):
+        """Test lookup of rules for a specific user agent."""
+        # Exact match.
+        self.assertEqual(
+            lookup_robots_rules(EXAMPLE_MAP, 'excite'),
+            EXAMPLE_MAP['excite']
+            )
+        # Prefix match.
+        self.assertEqual(
+            lookup_robots_rules(EXAMPLE_MAP, 'web'),
+            EXAMPLE_MAP['webcrawler']
+            )
+        # Case-insensitive match.
+        self.assertEqual(
+            lookup_robots_rules(EXAMPLE_MAP, 'UnHipBot'),
+            EXAMPLE_MAP['unhipbot']
+            )
+        # Default.
+        self.assertEqual(
+            lookup_robots_rules(EXAMPLE_MAP, 'unknown-bot'),
+            EXAMPLE_MAP['*']
+            )
+
+    def test_0600_match_path(self):
+        """Test the `path_allowed` function."""
+        rules_all = EXAMPLE_MAP['excite']
+        rules_none = EXAMPLE_MAP['unhipbot']
+        rules_some = EXAMPLE_MAP['*']
+        for path, expected in (
+                ('/', False),
+                ('/index.html', False),
+                ('/server.html', True),
+                ('/services/fast.html', True),
+                ('/orgo.gif', False),
+                ('/org/about.html', True),
+                ('/org/plans.html', False),
+                ('/~jim/jim.html', False),
+                ('/~mak/mak.html', True),
+                ):
+            self.assertTrue(path_allowed(path, rules_all))
+            self.assertFalse(path_allowed(path, rules_none))
+            self.assertEqual(path_allowed(path, rules_some), expected)
+
+if __name__ == '__main__':
+    unittest.main()

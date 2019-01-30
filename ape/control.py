62439cbfdeae020554f4776a192bc812074c5b3d
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-01-30 12:30:28 +0100

    Improve docstrings in 'control' module
    I also snuck in some minor code changes, to improve consistency.
    These should have no impact on how the code is currently used,
    but make it more robust to possible future changes.

--- ape/control.py
+++ ape/control.py
@@ -1,35 +1,64 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
+"""Models form controls.
+
+This module contains the `Control` class and its subclasses, which can be
+used to model input elements in an (HTML) form.
+"""
+
+# TODO: The subclasses currently have an almost 1:1 mapping to HTML,
+#       but I'm not sure that is necessary. For example SelectMultiple
+#       and Checkbox have the same submission functionality.
+#       And SelectSingle and RadioButtonGroup differ only in whether
+#       non-selection is possible; when adding support for the HTML5
+#       "required" attribute, SelectSingle can become functionally
+#       equivalent to RadioButtonGroup.
+
 class Control:
-    """A submittable element in a form.
-    The alternative name = None, value = None represents a control not being
-    part of the submission, for example an unchecked checkbox.
-    """
+    """Abstract base class for submittable elements in a form."""
 
     def has_alternative(self, name, value):
-        """Returns True iff the given name-value combination could be submitted
-        by this control.
-        Note that for free input controls, it is possible this method returns
-        True while the name-value pair is not in the sequence returned by the
-        "alternatives" method.
+        """Return `True` iff the given name-value combination could be
+        submitted by this control.
+
+        Note that for free-input controls, it is possible this method
+        returns `True` while the name-value pair is not in the sequence
+        returned by the `Control.alternatives` method.
         """
         raise NotImplementedError
 
     def alternatives(self):
-        """Returns a sequence containing all alternative name-value pairs of
-        the ways this control can be submitted.
-        For multiple-choice controls all possible alternatives are considered,
-        for free input controls there is an infinite number of alternatives, so
-        we just pick a few.
+        """Yield alternative name-value pairs of the ways this control
+        can be submitted.
+
+        For multiple-choice controls all possible alternatives are included.
+        For free-input controls there is an infinite number of alternatives,
+        so we just pick a few.
+
+        The alternative `(None, None)` represents a control not being part
+        of the submission, for example an unchecked checkbox.
         """
         raise NotImplementedError
 
 class SingleValueControl(Control):
+    """Control that produces at most one name-value combination.
+
+    Note that there can be any number of possible values, but in each
+    submission of the form, no more than one value is submitted for this
+    control.
+    """
 
     def __init__(self, name, value):
+        """Initialize control with the given name-value combination."""
         Control.__init__(self)
         self.name = name
+        """The name under which this control is submitted."""
         self.value = value
+        """The default value for this control.
+
+        Some control types can only submit this value,
+        other control types can submit other values as well.
+        """
 
     def has_alternative(self, name, value):
         return name == self.name and value == self.value
@@ -38,8 +67,7 @@ class SingleValueControl(Control):
         yield self.name, self.value
 
 class FileInput(SingleValueControl):
-    """A control for selecting and uploading files.
-    """
+    """Control for selecting and uploading files."""
 
     def has_alternative(self, name, value):
         # Any text could be submitted, so we only have to check the name.
@@ -53,9 +81,13 @@ class FileInput(SingleValueControl):
         yield self.name, ''
 
 class HiddenInput(SingleValueControl):
-    pass
+    """Control that is not visible to the user.
+
+    This control submits its default value.
+    """
 
 class TextField(SingleValueControl):
+    """Single-line text input."""
 
     def has_alternative(self, name, value):
         # Any text could be submitted, so we only have to check the name.
@@ -67,6 +99,7 @@ class TextField(SingleValueControl):
         yield self.name, 'ook' # librarian's choice
 
 class TextArea(SingleValueControl):
+    """Multi-line text input."""
 
     def has_alternative(self, name, value):
         # Any text could be submitted, so we only have to check the name.
@@ -78,6 +111,11 @@ class TextArea(SingleValueControl):
         yield self.name, 'Ook.\nOok? Ook!' # librarian's choice
 
 class Checkbox(SingleValueControl):
+    """Checkbox.
+
+    This control can submit its default value (box checked)
+    or nothing (box unchecked).
+    """
 
     def has_alternative(self, name, value):
         return (
@@ -90,6 +128,10 @@ class Checkbox(SingleValueControl):
         yield self.name, self.value # box checked
 
 class RadioButton(SingleValueControl):
+    """Single radio button.
+
+    Radio buttons must be combined in a `RadioButtonGroup` control.
+    """
 
     def has_alternative(self, name, value):
         assert False, 'radio button "%s" was not merged' % self.name
@@ -97,12 +139,14 @@ class RadioButton(SingleValueControl):
     def alternatives(self):
         assert False, 'radio button "%s" was not merged' % self.name
 
-class SubmitButton(SingleValueControl):
-    pass
-
 class RadioButtonGroup(Control):
+    """Multiple-choice control containing one or more radio buttons."""
 
     def __init__(self, buttons):
+        """Initialize a radio buttons group control containing `buttons`,
+        which must be a non-empty sequence of `RadioButton` objects.
+        """
+
         # Perform sanity check on input and gather values.
         name = buttons[0].name
         values = []
@@ -129,9 +173,24 @@ class RadioButtonGroup(Control):
         for value in self.values:
             yield self.name, value
 
+class SubmitButton(SingleValueControl):
+    """Single submit button.
+
+    All submit buttons in a form must be combined in a `SubmitButtons`
+    control.
+    """
+
 class SubmitButtons(Control):
+    """Pseudo-control which contains all submit buttons for a form.
+
+    Only one submit button can be used for submission;
+    this pseudo-control models the choice between submit buttons.
+    """
 
     def __init__(self, buttons):
+        """Initialize a submit buttons control containing `buttons`,
+        which must be a sequence of `SubmitButton` objects.
+        """
         Control.__init__(self)
         self.buttons = tuple((button.name, button.value) for button in buttons)
 
@@ -139,11 +198,13 @@ class SubmitButtons(Control):
         return (name, value) in self.buttons
 
     def alternatives(self):
-        return self.buttons
+        yield from self.buttons
 
 class SelectMultiple(SingleValueControl):
-    """Pseudo-control which represents an option in a <select> control where
-    multiple options can be active at the same time.
+    """Pseudo-control which represents an option in a `<select>` control
+    where multiple options can be active at the same time.
+
+    This type of control is typically shown in a browser as a list box.
     """
 
     def has_alternative(self, name, value):
@@ -157,13 +218,25 @@ class SelectMultiple(SingleValueControl):
         yield self.name, self.value # selected
 
 class SelectSingle(Control):
-    """A <select> control where one option can be active at the same time.
+    """`<select>` control where one option can be active at the same time.
+
+    This type of control is typically shown in a browser as a drop-down list.
     """
 
     def __init__(self, name, options):
+        """Initialize a single-choice `<select>` control.
+
+        Parameters:
+
+        name
+            The name under which this control is submitted.
+        options
+            Sequence of all possible values for this control.
+        """
+
         Control.__init__(self)
         self.name = name
-        self.options = options
+        self.options = tuple(options)
 
     def has_alternative(self, name, value):
         return (

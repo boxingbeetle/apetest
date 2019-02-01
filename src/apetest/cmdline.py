3649faeed2a744d369f9bd8a0dc00f128ecbff16
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-02-01 13:27:24 +0100

    Move command line entry point into `cmdline` module
    A wrapper script gets created for us during installation.

--- src/apetest/cmdline.py
+++ src/apetest/cmdline.py
@@ -2,14 +2,19 @@
 
 """Command line interface."""
 
+from argparse import ArgumentParser
+import logging
 from os import getcwd
 from urllib.parse import urljoin, urlparse
 
-from apetest.checker import PageChecker
-from apetest.plugin import PluginCollection
+from apetest.checker import Accept, PageChecker
+from apetest.plugin import (
+    PluginCollection, add_plugin_arguments, create_plugins, load_plugins
+    )
 from apetest.report import Scribe
 from apetest.request import Request
 from apetest.spider import spider_req
+from apetest.version import VERSION_STRING
 
 def detect_url(arg):
     """Attempt to turn a command line argument into a full URL."""
@@ -83,3 +88,57 @@ def run(url, report_file_name, accept, plugins=()):
         return 0
     finally:
         plugins.close()
+
+def main():
+    """Parse command line arguments and call `run` with the results.
+
+    This is the entry point that gets called by the wrapper script.
+    """
+
+    # Register core arguments.
+    parser = ArgumentParser(
+        description='Automated Page Exerciser: '
+                    'smarter-than-monkey testing for web apps',
+        epilog='This is a test tool; do not use on production sites.'
+        )
+    parser.add_argument(
+        'url', metavar='URL|PATH',
+        help='web app/site to check'
+        )
+    parser.add_argument(
+        '--accept', type=str, choices=('any', 'html'), default='any',
+        help='accept serialization: any (HTML or XHTML; default) or HTML only'
+        )
+    parser.add_argument(
+        'report', metavar='REPORT',
+        help='file to write the HTML report to'
+        )
+    parser.add_argument(
+        '-v', '--verbose', action='count', default=0,
+        help='increase amount of logging, can be passed multiple times'
+        )
+    parser.add_argument(
+        '-V', '--version', action='version', version='APE %s' % VERSION_STRING
+        )
+
+    # Let plugins register their arguments.
+    plugin_modules = tuple(load_plugins())
+    for module in plugin_modules:
+        add_plugin_arguments(module, parser)
+
+    args = parser.parse_args()
+
+    level_map = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
+    level = level_map.get(args.verbose, logging.DEBUG)
+    logging.basicConfig(level=level, format='%(levelname)s: %(message)s')
+
+    # Instantiate plugins.
+    plugins = []
+    for module in plugin_modules:
+        try:
+            plugins += create_plugins(module, args)
+        except Exception: # pylint: disable=broad-except
+            return 1
+
+    accept = Accept[args.accept.upper()]
+    return run(args.url, args.report, accept, plugins)

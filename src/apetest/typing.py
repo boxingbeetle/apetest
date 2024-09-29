a8994477d685b2b80a6d6e63cb70d97412f2a150
Maarten ter Huurne <maarten@treewalker.org>
2024-09-29 21:03:12 +0200

    Eliminate unnecessary use of `Union`
    Only mypy will see what is Inside a `TYPE_CHECKING` block.

--- src/apetest/typing.py
+++ src/apetest/typing.py
@@ -7,11 +7,11 @@ Various helpers for static type checking.
 from __future__ import annotations
 
 from logging import Logger, LoggerAdapter
-from typing import TYPE_CHECKING, Any, Union
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     # pylint: disable=unsubscriptable-object
-    LoggerT = Union[Logger, LoggerAdapter[Any]]
+    LoggerT = Logger | LoggerAdapter[Any]
     LoggerBase = LoggerAdapter[Logger]
 else:
     LoggerT = LoggerAdapter
